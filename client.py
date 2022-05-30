fe430c78f80b76c0dd90ecba91975bb1312c1973
종박 <78944379+jongpark1234@users.noreply.github.com>
2022-05-30 18:59:09 +0900

    강준호 뚝배기 함 깨보자!!

---
+++ client.py
@@ -0,0 +1,48 @@
+import socket
+
+HOST = '10.80.162.18'
+
+PORT = 8000
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # 소켓을 만든다.
+
+client_socket.connect((HOST, PORT)) # connect함수로 접속을 한다.
+
+try:
+
+    print('** 서버에 접속하였습니다. **')
+    
+    while ([input('ID: '), input('PASS: ')] != ['admin', '1234']):
+        print('** ID 또는 PASS가 틀렸습니다.! **')
+
+    print('** FTP 서버에 접속하였습니다. **')
+
+    while True:
+
+        msg = input() # 보낼 메세지 입력
+        
+        data = msg.encode() # 메시지를 바이너리(byte)형식으로 변환한다.
+        
+        length = len(data) # 메시지 길이를 구한다.
+        
+        client_socket.sendall(length.to_bytes(4, byteorder = 'little')) # server로 리틀 엔디언 형식으로 데이터 길이를 전송한다.
+        
+        client_socket.sendall(data) # 데이터를 전송한다.
+        
+        data = client_socket.recv(4) # server로 부터 전송받을 데이터 길이를 받는다.
+        
+        length = int.from_bytes(data, 'little') # 데이터 길이는 리틀 엔디언 형식으로 int를 변환한다.
+        
+        data = client_socket.recv(length) # 데이터 길이를 받는다.
+        
+        msg = data.decode() # 데이터를 수신한다.
+        
+        print('Received from : ', msg) # 데이터를 출력한다.
+
+except:
+
+    print('연결 종료')
+
+finally:
+
+    client_socket.close()
---
+++ server.py
@@ -0,0 +1,61 @@
+import socket, threading
+
+def binder(client_socket, address): # binder함수는 서버에서 accept가 되면 생성되는 socket 인스턴스를 통해 client로 부터 데이터를 받으면 echo형태로 재송신하는 메소드이다.
+
+    print('Connected by', address) # 커넥션이 되면 접속 주소가 나온다.
+
+    try:
+        while True: # 접속 상태에서는 클라이언트로 부터 받을 데이터를 무한 대기한다.
+            data = client_socket.recv(4) # socket의 recv함수는 연결된 소켓으로부터 데이터를 받을 대기하는 함수입니다. 최초 4바이트를 대기합니다.
+
+            length = int.from_bytes(data, 'little') # 최초 4바이트는 전송할 데이터의 크기이다. 그 크기는 little 엔디언으로 byte에서 int형식으로 변환한다.
+
+            data = client_socket.recv(length) # 다시 데이터를 수신한다.
+            
+            msg = data.decode() # 수신된 데이터를 str형식으로 decode한다.
+            
+            print('Received from', msg) # 수신된 메시지를 콘솔에 출력한다.
+            
+            msg = 'echo : ' + msg # 수신된 메시지 앞에 「echo:」 라는 메시지를 붙힌다.
+            
+            data = msg.encode() # 바이너리(byte)형식으로 변환한다.
+            
+            length = len(data) # 바이너리의 데이터 사이즈를 구한다.
+            
+            client_socket.sendall(length.to_bytes(4, byteorder = 'little')) # 데이터 사이즈를 little 엔디언 형식으로 byte로 변환한 다음 전송한다.
+            
+            client_socket.sendall(data) # 데이터를 클라이언트로 전송한다.
+
+    except: # 접속이 끊기면 except가 발생한다.
+
+        print('except : ' + address)
+
+    finally: # 접속이 끊기면 socket 리소스를 닫는다.
+
+        client_socket.close()
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # 소켓을 만든다.
+
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # 소켓 레벨과 데이터 형태를 설정한다.
+
+server_socket.bind(('10.80.162.18', 8000))
+
+server_socket.listen() # server 설정이 완료되면 listen를 시작한다.
+
+try:
+
+    while True: # 서버는 여러 클라이언트를 상대하기 때문에 무한 루프를 사용한다.
+
+        client_socket, addr = server_socket.accept() # client로 접속이 발생하면 accept가 발생한다. 그럼 client 소켓과 addr(주소)를 튜플로 받는다.
+
+        Thread = threading.Thread(target = binder, args = (client_socket, addr)) # Thread를 이용해서 client 접속 대기를 만들고 다시 accept로 넘어가서 다른 client를 대기한다.
+
+        Thread.start()
+
+except:
+
+    print('server close')
+
+finally: # 에러가 발생하면 서버 소켓을 닫는다.
+
+    server_socket.close()
